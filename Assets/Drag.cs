c763c69a19ba7b2cac950aac76df4e2282183c05
James Benson <james.benson@ioof.com.au>
2019-12-28 23:14:16 +1100

    Add basic selection box UI.

---
+++ Assets/Drag.cs
@@ -0,0 +1,48 @@
+﻿// Enable the selection of a ScrollView which can be dragged.
+// This script only chooses the ScrollView child of the Canvas.
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Drag : MonoBehaviour, IBeginDragHandler
+{
+    private GameObject m_DraggingIcon;
+
+    public void Start()
+    {
+        m_DraggingIcon = new GameObject("icon");
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        Debug.Log("OnBeginDrag");
+
+        var canvas = FindInParents<Canvas>(gameObject);
+        if (canvas == null)
+            return;
+
+        m_DraggingIcon.transform.SetParent(canvas.transform, false);
+        m_DraggingIcon.transform.SetAsLastSibling();
+
+        Debug.Log("Dragging started");
+    }
+
+    // locate and return the Canvas
+    static public T FindInParents<T>(GameObject go) where T : Component
+    {
+        if (go == null) return null;
+        var comp = go.GetComponent<T>();
+
+        if (comp != null)
+            return comp;
+
+        Transform t = go.transform.parent;
+        while (t != null && comp == null)
+        {
+            comp = t.gameObject.GetComponent<T>();
+            t = t.parent;
+        }
+        return comp;
+    }
+}
---
+++ Assets/SelectionBox.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+public class SelectionBox : MonoBehaviour
+{
+    private Vector3 mouseDownPoint;
+    private Vector3 mouseDragEnd;
+
+    [SerializeField]
+    private RectTransform selectionRect;
+    [SerializeField]
+    private RectTransform selectFill;
+
+    private void Start() {
+        selectBox.gameObject.SetActive(false);
+        selectFill.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDownPoint = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            mouseDragEnd = Input.mousePosition;
+            if (!selectBox.gameObject.activeInHierarchy)
+            {
+                selectBox.gameObject.SetActive(true);
+                selectFill.gameObject.SetActive(true);
+            }
+
+            float dx = Mathf.Abs(mouseDownPoint.x - mouseDragEnd.x);
+            float dy = Mathf.Abs(mouseDownPoint.y - mouseDragEnd.y);
+
+            selectBox.sizeDelta = new Vector2(dx, dy);
+            selectFill.sizeDelta = new Vector2(dx, dy);
+
+            Vector3 centre = (mouseDownPoint + mouseDragEnd) / 2f;
+            centre.z = 0f;
+            selectBox.position = centre;
+            selectFill.position = centre;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectBox.gameObject.SetActive(false);
+            selectFill.gameObject.SetActive(false);
+        }
+    }
+}
